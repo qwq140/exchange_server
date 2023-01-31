48a5dcb9f793a4b451e3d870ef341bbeffdbd08a
qwq140 <qwq140@gmail.com>
2023-01-31 12:04:38 +0900

    init

---
+++ src/main/java/com/pjay/exchange_server/ExchangeServerApplication.java
@@ -0,0 +1,13 @@
+package com.pjay.exchange_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pjay/exchange_server/ExchangeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjay.exchange_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
